73604d034f251b09966a90f50828521776a777f6
djm <djm>
2007-02-08 02:30:41 +0000

    - (djm) Generalised the generated code: a FSM is now an opaque structure    rather than having only one per generated source file  - (djm) Add a regress test for the multiple initial-state case

--- regress/t2.c
+++ regress/t2.c
@@ -9,16 +9,21 @@
 #include <string.h>
 #include <assert.h>
 
-#include "t1_fsm.h"
+#include "t2_fsm.h"
 
 int
 main(int argc, char **argv)
 {
 	struct fsm *fsm = NULL;
 
-	assert((fsm = state_init(NULL, 0)) != NULL);
-	assert(state_current(fsm) == T1);
-	assert(strcmp(state_ntop(state_current(fsm)), "T1") == 0);
+	assert((fsm = state_init(T1b, NULL, 0)) != NULL);
+	state_free(fsm);
+	assert((fsm = state_init(T2, NULL, 0)) == NULL);
+	assert((fsm = state_init(T3, NULL, 0)) == NULL);
+	assert((fsm = state_init(T4, NULL, 0)) == NULL);
+	assert((fsm = state_init(T1a, NULL, 0)) != NULL);
+	assert(state_current(fsm) == T1a);
+	assert(strcmp(state_ntop(state_current(fsm)), "T1a") == 0);
 	assert(state_advance(fsm, T2, NULL, 0) == 0);
 	assert(state_current(fsm) == T2);
 	assert(strcmp(state_ntop(state_current(fsm)), "T2") == 0);
@@ -35,7 +40,7 @@ main(int argc, char **argv)
 	assert(state_advance(fsm, T4, NULL, 0) == 0);
 	assert(state_current(fsm) == T4);
 	assert(strcmp(state_ntop(state_current(fsm)), "T4") == 0);
-	assert(state_advance(fsm, T1, NULL, 0) == -1);
+	assert(state_advance(fsm, T1b, NULL, 0) == -1);
 	assert(state_current(fsm) == T4);
 	state_free(fsm);
 	return 0;
